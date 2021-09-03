edd6807f7f3e6ffd67cc2b5926235d4c3676cf83
Arman <aisenov1995@gmail.com>
2021-09-03 11:48:11 +0500

    Выполнено 8 упр. 19 задания ReactJS

--- src/homework/5week3day2part/5week3day2part.js
+++ src/homework/5week3day2part/5week3day2part.js
@@ -182,4 +182,47 @@ const AuthorizationForm = () => {
     )
 }
 
-export default AuthorizationForm
+const VisibleSquare = () => {
+
+    const[isShowSquare, setIsShowSquare] = React.useState(false)
+    const handleOnChangeInputSquare = (event) => {
+        setIsShowSquare(event.target.checked)
+    }
+    return(
+        <>
+            <input type="checkbox" onChange={handleOnChangeInputSquare} checked={isShowSquare}/>
+            {isShowSquare ? null : <Square/>}
+        </>
+    )
+}
+const Square = () => <div style={{width:'100px', height: '100px', backgroundColor: 'red'}}>Red square</div>
+
+
+const SizeText = () => {
+
+    const firstSize = 8
+    const secondSize = 16
+    const thirdSize = 24
+    const fourthSize = 36
+
+    const [selectValue, setSelectValue] = React.useState(firstSize)
+    const handleOnChangeSize = (event) => {
+        setSelectValue(event.target.value)
+    }
+    console.log(selectValue)
+
+    return(
+        <>
+            <p style={{fontSize:`${selectValue}px`}}>Some text here</p>
+            <select onChange={handleOnChangeSize} value={selectValue}>
+                <option value={firstSize}>{firstSize}</option>
+                <option value={secondSize}>{secondSize}</option>
+                <option value={thirdSize}>{thirdSize}</option>
+                <option value={fourthSize}>{fourthSize}</option>
+            </select>
+        </>
+
+    )
+}
+
+export default SizeText
